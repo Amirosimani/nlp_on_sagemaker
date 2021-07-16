3a1c9004252fea7f42d516864527356b8d837111
amir <imaniai@amazon.com>
2021-07-16 21:17:16 +0000

    custom data + metrics + spot

--- src/train.py
+++ src/train.py
@@ -1,36 +1,49 @@
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
+from transformers.trainer_utils import get_last_checkpoint
 
-import argparse
-import logging
-import os
-import sys
-
-import tensorflow as tf
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from datasets import load_dataset
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+import logging
+import sys
+import argparse
+import os
+from datasets import load_dataset
+from transformers import AutoTokenizer
 
+
+# Set up logging
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.getLevelName("INFO"),
+    handlers=[logging.StreamHandler(sys.stdout)],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
 
 if __name__ == "__main__":
 
+    logger.info(sys.argv)
+
     parser = argparse.ArgumentParser()
 
-    # Hyperparameters sent by the client are passed as command-line arguments to the script.
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
     parser.add_argument("--epochs", type=int, default=3)
-    parser.add_argument("--train-batch-size", type=int, default=16)
-    parser.add_argument("--eval-batch-size", type=int, default=8)
+    parser.add_argument("--train-batch-size", type=int, default=32)
+    parser.add_argument("--eval-batch-size", type=int, default=64)
+    parser.add_argument("--warmup_steps", type=int, default=500)
     parser.add_argument("--model_name", type=str)
     parser.add_argument("--learning_rate", type=str, default=5e-5)
-    parser.add_argument("--do_train", type=bool, default=True)
-    parser.add_argument("--do_eval", type=bool, default=True)
+    parser.add_argument("--output_dir", type=str)
 
     # Data, model, and output directories
-    parser.add_argument("--output_data_dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
-    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--n_gpus", type=str, default=os.environ["SM_NUM_GPUS"])
     parser.add_argument("--training_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
     parser.add_argument("--test_dir", type=str, default=os.environ["SM_CHANNEL_TEST"])
 
     args, _ = parser.parse_known_args()
-
+    
     # Set up logging
     logger = logging.getLogger(__name__)
 
@@ -39,15 +52,13 @@ if __name__ == "__main__":
         handlers=[logging.StreamHandler(sys.stdout)],
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
-
-    # Load model and tokenizer
-    model = TFAutoModelForSequenceClassification.from_pretrained(args.model_name)
+   
+    # download tokenizer
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-
+    
     # Load dataset
     train_file = f"{args.training_dir}/train.csv"
     validate_file = f"{args.test_dir}/validate.csv"
-#     train_dataset, test_dataset = load_dataset("imdb", split=["train", "test"])
     dataset = load_dataset('csv', data_files={'train': train_file,
                                              'test': validate_file})
     
@@ -56,70 +67,71 @@ if __name__ == "__main__":
     logger.info(f" loaded train_dataset length is: {len(train_dataset)}")
     logger.info(f" loaded test_dataset length is: {len(test_dataset)}")
 
-    # Preprocess train dataset
-    train_dataset = train_dataset.map(
-        lambda e: tokenizer(e["text"], truncation=True, padding="max_length"), batched=True
+
+    # tokenizer helper function
+    def tokenize(batch):
+        return tokenizer(batch['text'], padding='max_length', truncation=True)
+
+    # tokenize dataset
+    train_dataset = train_dataset.map(tokenize, batched=True)
+    test_dataset = test_dataset.map(tokenize, batched=True)
+    
+
+    # set format for pytorch
+    train_dataset =  train_dataset.rename_column("label", "labels")
+    train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+    test_dataset = test_dataset.rename_column("label", "labels")
+    test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+
+    logger.info(f" loaded train_dataset length is: {len(train_dataset)}")
+    logger.info(f" loaded test_dataset length is: {len(test_dataset)}")
+
+    # compute metrics function for binary classification
+    def compute_metrics(pred):
+        labels = pred.label_ids
+        preds = pred.predictions.argmax(-1)
+        precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average="weighted")
+        acc = accuracy_score(labels, preds)
+        return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
+
+    # download model from model hub
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name, num_labels=5)
+
+    # define training args
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        num_train_epochs=args.epochs,
+        per_device_train_batch_size=args.train_batch_size,
+        per_device_eval_batch_size=args.eval_batch_size,
+        warmup_steps=args.warmup_steps,
+        evaluation_strategy="epoch",
+        logging_dir=f"{args.output_data_dir}/logs",
+        learning_rate=float(args.learning_rate),
     )
-    train_dataset.set_format(type="tensorflow", columns=["input_ids", "attention_mask", "label"])
 
-    train_features = {
-        x: train_dataset[x].to_tensor(default_value=0, shape=[None, tokenizer.model_max_length])
-        for x in ["input_ids", "attention_mask"]
-    }
-    tf_train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_dataset["label"])).batch(
-        args.train_batch_size
+    # create Trainer instance
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
     )
 
-    # Preprocess test dataset
-    test_dataset = test_dataset.map(
-        lambda e: tokenizer(e["text"], truncation=True, padding="max_length"), batched=True
-    )
-    test_dataset.set_format(type="tensorflow", columns=["input_ids", "attention_mask", "label"])
+    # train model
+    if get_last_checkpoint(args.output_dir) is not None:
+        logger.info("***** continue training *****")
+        trainer.train(resume_from_checkpoint=args.output_dir)
+    else:
+        trainer.train()
+#     # evaluate model
+#     eval_result = trainer.evaluate(eval_dataset=test_dataset)
 
-    test_features = {
-        x: test_dataset[x].to_tensor(default_value=0, shape=[None, tokenizer.model_max_length])
-        for x in ["input_ids", "attention_mask"]
-    }
-    tf_test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_dataset["label"])).batch(
-        args.eval_batch_size
-    )
+#     # writes eval result to file which can be accessed later in s3 ouput
+#     with open(os.path.join(args.output_data_dir, "eval_results.txt"), "w") as writer:
+#         print(f"***** Eval results *****")
+#         for key, value in sorted(eval_result.items()):
+#             writer.write(f"{key} = {value}\n")
 
-    # fine optimizer and loss
-    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-    # Training
-    if args.do_train:
-
-        train_results = model.fit(tf_train_dataset, epochs=args.epochs, batch_size=args.train_batch_size)
-        logger.info("*** Train ***")
-
-        output_eval_file = os.path.join(args.output_data_dir, "train_results.txt")
-
-        with open(output_eval_file, "w") as writer:
-            logger.info("***** Train results *****")
-            logger.info(train_results)
-            for key, value in train_results.history.items():
-                logger.info("  %s = %s", key, value)
-                writer.write("%s = %s\n" % (key, value))
-
-    # Evaluation
-    if args.do_eval:
-
-        result = model.evaluate(tf_test_dataset, batch_size=args.eval_batch_size, return_dict=True)
-        logger.info("*** Evaluate ***")
-
-        output_eval_file = os.path.join(args.output_data_dir, "eval_results.txt")
-
-        with open(output_eval_file, "w") as writer:
-            logger.info("***** Eval results *****")
-            logger.info(result)
-            for key, value in result.items():
-                logger.info("  %s = %s", key, value)
-                writer.write("%s = %s\n" % (key, value))
-
-    # Save result
-    model.save_pretrained(args.model_dir)
-    tokenizer.save_pretrained(args.model_dir)
+    # Saves the model to s3
+    trainer.save_model(args.model_dir)
